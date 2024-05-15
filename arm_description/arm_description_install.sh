c7f4f1deda01f62c44adcf5af04e2df3b20112ae
omar salem <omar.gamil.salem@gmail.com>
2024-05-15 10:20:23 +1000

    refactor

--- arm_description/arm_description_install.sh
+++ arm_description/arm_description_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+cd arm_ws
 colcon build --packages-select arm_description
 source install/setup.bash
 ros2 launch arm_description display.launch.py
 
-
+cd arm_ws
 colcon build --packages-select arm_description
 source install/setup.bash
 ros2 launch arm_description gazebo.launch.py
