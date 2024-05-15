0311744501e19148fa7ed0568abe7fa57f450f9f
omar salem <omar.gamil.salem@gmail.com>
2024-05-15 11:16:19 +1000

    updated control test command

--- arm_control/arm_control_install.sh
+++ arm_control/arm_control_install.sh
@@ -11,4 +11,4 @@ source install/setup.bash
 ros2 launch arm_control gazebo_control.launch.py
 
 
-ros2 topic pub --once /joint_trajectory_controller/joint_trajectory trajectory_msgs/msg/JointTrajectory '{joint_names:["shoulder_joint"],points: [{positions:[1]}]}'
+ros2 topic pub --once /joint_trajectory_controller/joint_trajectory trajectory_msgs/msg/JointTrajectory '{joint_names:["base_joint","shoulder_joint"],points: [{positions:[1,2]}]}'
