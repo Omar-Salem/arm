1cb84361d1ba0dd79aa680d623b269e7bea45d77
omar salem <omar.gamil.salem@gmail.com>
2024-05-17 08:28:08 +1000

    -virtual_joint in urdf -removed VIRTUAL JOINT in srdf -updated rviz config -install.sh

--- arm_moveit_config/arm_moveit_config_install.sh
+++ arm_moveit_config/arm_moveit_config_install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 cd arm_ws
-colcon build --packages-select arm_moveit_config
-source install/setup.bash
-ros2 launch arm_moveit_config demo.launch.py
+rm -rf build log install
+colcon build
 
 
 cd arm_ws
-
 killall -9 gazebo & killall -9 gzserver & killall -9 gzclient
 killall -9 rviz
 colcon build --packages-select arm_moveit_config
