face36dd24f1b2e22bfaaf0f1fa8974f2e2a4d01
omar salem <omar.gamil.salem@gmail.com>
2024-06-02 08:38:06 +1000

    -moveit_config

---
+++ arm_moveit_config/arm_moveit_config_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ros2 launch moveit_setup_assistant setup_assistant.launch.py
+
+cd arm_ws
+rm -rf build log install
+colcon build
+
+cd arm_ws
+killall -9 rviz
+colcon build --packages-select arm_moveit_config
+source install/setup.bash
+ros2 launch arm_moveit_config moveit.launch.py
+
+
+cd arm_ws
+killall -9 gazebo & killall -9 gzserver & killall -9 gzclient
+killall -9 rviz
+colcon build --packages-select arm_moveit_config
+source install/setup.bash
+ros2 launch arm_moveit_config gazebo.launch.py
